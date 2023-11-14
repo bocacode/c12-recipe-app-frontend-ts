24ba37db57731e9128359ba1150237ad0518b4fd
Diego F Garcia <gadifer54@gamil>
2023-11-14 16:48:35 -0500

    updated footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,12 @@
 
-export default function Footer(){
-	return(
-	
-	<footer className="mt-auto text-white-50">
-		<p>Cover template for <a href= "Copyright © {new Date().getFullYear()}| All Rights Reserved" className="text-white">Bootstrap</a>, by <a href="https://twitter.com/mdo" class="text-white">@mdo</a>.</p>
-	  </footer>
+export default function Footer() {
+	return (
+
+		<footer className="mt-auto text-white-50">
+			<p> Copyright © {new Date().getFullYear()}| All Rights Reserved</p >
+		</footer>
 	)
-	
-	}
-	
+
+}
+
 
