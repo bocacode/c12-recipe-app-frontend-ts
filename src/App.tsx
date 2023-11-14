88c64ce60dc87ce1bcff940daf7fffa5b2c6e487
John Savin <johnsavin@proton.me>
2023-11-14 15:22:54 -0500

    nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
 import './App.css'
+import Header from './components/Header'
+
+
+
 
 function App() {
 	return (
 		<BrowserRouter>
-    {/* <Header/>  */}
+	
+    <Header /> 
 			<Routes>
         <Route path='/' element={<button className='btn btn-primary'>This is home</button>} /> 
       </Routes>
