475e27b7500821c808fa09aa6e995c7bb9707bdd
Debbie Davila <debbie@debbiedavila.com>
2023-11-14 15:56:11 -0500

    Header assignment

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
+import Header from './components/Header'
 import './App.css'
 
 function App() {
 	return (
 		<BrowserRouter>
-    {/* <Header/>  */}
+    <Header/> 
 			<Routes>
         <Route path='/' element={<button className='btn btn-primary'>This is home</button>} /> 
       </Routes>
