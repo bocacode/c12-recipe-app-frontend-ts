d2f9b5f071ec1c4afa50c5bad6a6f6e24ed5ded1
jonathans199 <jso1919@gmail.com>
2023-11-14 16:42:13 -0500

    updted home

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+	return (
+		<main className='px-3'>
+			<h1>Home</h1>
+			<p className='lead'>Welcome To The Home Page</p>
+			<p className='lead'>
+				<a href='#' className='btn btn-lg btn-light fw-bold border-white bg-white'>
+					Learn more
+				</a>
+			</p>
+		</main>
+	)
+}
