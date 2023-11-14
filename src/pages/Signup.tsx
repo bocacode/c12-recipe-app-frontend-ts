a75f1f667506287dd6702830ae461c62059530ff
Michael Fisher <mafish88@icloud.com>
2023-11-14 16:35:05 -0500

    signup

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,7 +1,21 @@
-export default function Signup() {
+
+import Form from 'react-bootstrap/Form'
+
+
+ function TextControlsExample() {
 	return (
-		<>
-			<h2>Signup here</h2>
-		</>
+		<Form>
+		<Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+		  <Form.Label>Email address</Form.Label>
+		  <Form.Control type="email" placeholder="name@example.com" />
+		</Form.Group>
+		<Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+		  <Form.Label>Password</Form.Label>
+		  <Form.Control as="textarea" rows={3} />
+		</Form.Group>
+	  </Form>
 	)
 }
+
+
+export default TextControlsExample;
